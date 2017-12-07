a81211ad8297000e730bfcc65d9352c07a6c7fc2
DanielHood <mrdhood@gmail.com>
2017-12-07 11:20:05 -0500

    Added color and sport to the fields list.

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password',
-        'address', 'phone'
+        'address', 'phone', 'sport', 'color'
     ];
 
     /**
